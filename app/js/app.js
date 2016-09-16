617e0ae4029a014dcaedba2bcaa4712937341ec1
MoreNaruto <morenaruto007@gmail.com>
2016-09-16 03:56:09 -0500

    Setting up modal

--- app/js/app.js
+++ app/js/app.js
@@ -4,3 +4,13 @@ angular.module('labelInsightApp', [
     'labelInsightApp.controllers'
 ]);
 
+var modalStarter = (function() {
+    for (var i = 1; i <= 25; i++) {
+        $('#photo-starter-1').on('click', function() {
+            $('#photo-modal-1').modal('show');
+        });
+    }
+
+});
+
+modalStarter();
